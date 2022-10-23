ef355a596449c1ad6fdb9d56b47d5f0b524d2d80
FernandoLorca <florcapinto@gmail.com>
2022-10-23 19:10:07 -0300

    push to git remote

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,13 @@
 "use strict";
 
-const numbers = document.querySelectorAll(".compoment__rates--numbersBox");
-const button = document.querySelector(".button");
+const mainComponent = document.getElementById("mainComponent");
+const thanksComponent = document.getElementById("thanksYou");
+const button = document.getElementById("button");
+const rates = document.querySelectorAll(
+  ".compoment__rates--numberBox-numbers > button"
+);
+
+button.addEventListener("click", () => {
+  thanksComponent.classList.remove("hidden");
+  mainComponent.classList.add("hidden");
+});
